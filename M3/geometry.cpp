48df6a509ec7f5b0798df05618841ac8b4f0810e
15446-rus75 <vladabramov06@yandex.ru>
2025-10-19 14:39:00 +0300

    some Circle fixes

--- M3/geometry.cpp
+++ M3/geometry.cpp
@@ -1,11 +1,17 @@
 #include "geometry.hpp"
+#include <stdexcept>
 
 abramov::Circle::Circle(const std::string &s, int rad, int xcoord, int ycoord):
   name(s),
   r(rad),
   x(xcoord),
   y(ycoord)
-{}
+{
+  if (r <= 0)
+  {
+    throw std::logic_error("Radius must be positive");
+  }
+}
 
 int abramov::Circle::getRadius() const noexcept
 {
--- M3/geometry.hpp
+++ M3/geometry.hpp
@@ -1,22 +1,25 @@
 #ifndef GEOMETRY_HPP
 #define GEOMETRY_HPP
 #include <string>
-#include <iostream>
+#include "shape.hpp"
 
 namespace abramov
 {
-  struct Circle
+  struct Circle final: Shape
   {
     Circle(const std::string &s, int rad, int xcoord, int ycoord);
+    ~Circle() override = default;
     int getRadius() const noexcept;
     int getX() const noexcept;
     int getY() const noexcept;
-    std::ostream &printInfo(std::ostream &out = std::cout) const noexcept;
+    std::ostream &printInfo(std::ostream &out = std::cout) const noexcept override;
   private:
     std::string name;
     int r;
     int x;
     int y;
+
+    void swap(Circle &other) noexcept;
   };
 }
 #endif
