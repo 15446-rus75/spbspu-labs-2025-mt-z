6612169bbb41e4f67628fb54b217d2641d800301
15446-rus75 <vladabramov06@yandex.ru>
2026-04-17 22:20:38 +0300

    add parsing args

--- M2/main.cpp
+++ M2/main.cpp
@@ -1,17 +1,34 @@
+#include <cstring>
 #include <iostream>
 
 int main(int argc, char **argv)
 {
   long long int tries = 0;
   long long int seed = 0;
-  if (argc >= 2 && argc <= 3)
+  if (argc >= 4 && argc <= 5)
   {
     try
     {
       tries = std::stoll(argv[1]);
-      if (argc == 3)
+      int index = 2;
+      if (argc == 5)
       {
+        index = 3;
         seed = std::stoll(argv[2]);
+      }
+      if (strstr(argv[index], "--schedule="))
+      {}
+      else
+      {
+        std::cerr << "--schedule is not found\n";
+        return 1;
+      }
+      if (strstr(argv[++index], "--chunk-size="))
+      {}
+      else
+      {
+        std::cerr << "--chunk-size is not found\n";
+        return 1;
       }
     }
     catch (const std::exception &e)
