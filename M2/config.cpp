f75f497ce59eaa866719596ffebeb84d42e4d77c
15446-rus75 <vladabramov06@yandex.ru>
2026-04-20 12:47:38 +0300

    add AreaConfig

--- M2/config.cpp
+++ M2/config.cpp
@@ -19,12 +19,66 @@ abramov::Config::Config(const std::string &sch, size_t ch):
   }
 }
 
-std::string abramov::Config::getSchedule() const
+omp_sched_t abramov::Config::getSchedule() const
 {
-  return schedule;
+  if (schedule == "static")
+  {
+    return omp_sched_static;
+  }
+  if (schedule == "dynamic")
+  {
+    return omp_sched_dynamic;
+  }
+  if (schedule == "guided")
+  {
+    return omp_sched_guided;
+  }
+  return omp_sched_auto;
 }
 
 size_t abramov::Config::getChunk() const noexcept
 {
   return chunk;
+}
+
+void abramov::setSchedule(const Config &con)
+{
+  omp_set_schedule(con.getSchedule(), con.getChunk());
+}
+
+void abramov::showSchedule(const Config &con)
+{
+  std::cout << con.getSchedule() << ' ' << con.getChunk() << '\n';
+}
+
+abramov::AreaConfig::AreaConfig(int radius, long long int tr, long long int s, int t):
+  r(radius),
+  tries(tr),
+  seed(s),
+  ths(t)
+{
+  if (r <= 0 || tr <= 0 || s < 0 || t < 0)
+  {
+    throw std::logic_error("R, tries, seed, threads must be positive\n");
+  }
+}
+
+int abramov::AreaConfig::getR() const noexcept
+{
+  return r;
+}
+
+long long int abramov::AreaConfig::getTries() const noexcept
+{
+  return tries;
+}
+
+long long int abramov::AreaConfig::getSeed() const noexcept
+{
+  return seed;
+}
+
+int abramov::AreaConfig::getThs() const noexcept
+{
+  return ths;
 }
--- M2/config.hpp
+++ M2/config.hpp
@@ -2,6 +2,7 @@
 #define CONFIG_HPP
 #include <string>
 #include <stdexcept>
+#include <omp.h>
 
 namespace abramov
 {
@@ -9,12 +10,29 @@ namespace abramov
   {
     Config();
     Config(const std::string &sch, size_t ch);
-    std::string getSchedule() const;
+    omp_sched_t getSchedule() const;
     size_t getChunk() const noexcept;
   private:
     std::string schedule;
     size_t chunk;
   };
+
+  void setSchedule(const Config &con);
+  void showSchedule(const Config &con);
+
+  struct AreaConfig
+  {
+    AreaConfig(int radius, long long int tr, long long int s, int t);
+    int getR() const noexcept;
+    long long int getTries() const noexcept;
+    long long int getSeed() const noexcept;
+    int getThs() const noexcept;
+  private:
+    int r;
+    long long int tries;
+    long long int seed;
+    int ths;
+  }
 }
 
 #endif
