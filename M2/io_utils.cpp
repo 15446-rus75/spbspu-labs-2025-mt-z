54c20d78377707442cdb7fea51355aba006a05a3
15446-rus75 <vladabramov06@yandex.ru>
2026-04-20 12:51:36 +0300

    add func-wrapper

---
+++ M2/io_utils.cpp
@@ -0,0 +1,6 @@
+#include "io_utils.hpp"
+
+void abramov::printAreaOf(const AreaConfig &areacon)
+{
+  std::cout << getArea(areacon);
+}
---
+++ M2/io_utils.hpp
@@ -0,0 +1,9 @@
+#ifndef IO_UTILS_HPP
+#define IO_UTILS_HPP
+
+#include <iostream>
+#include "math_utils.hpp"
+
+void printAreaOf(const AreaConfig &areacon);
+
+#endif
